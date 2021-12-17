638a74cd1030e7af0073f65fe54cb6f895614011
Natalia Petyaeva <npetyaeva@gmail.com>
2021-12-17 18:00:09 +0300

    Modified .appveyor.yml

--- src/test/java/ru/netology/postmanecho/MobileBankApiTest.java
+++ src/test/java/ru/netology/postmanecho/MobileBankApiTest.java
@@ -9,15 +9,11 @@ public class MobileBankApiTest {
 
     @Test
     void shouldReturn() {
-        // Given - When - Then
-        // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
-                // Выполняемые действия
+                .body("some data")
                 .when()
                 .post("/post")
-                // Проверки
                 .then()
                 .statusCode(200)
                 .body("data", equalTo("some data"))
