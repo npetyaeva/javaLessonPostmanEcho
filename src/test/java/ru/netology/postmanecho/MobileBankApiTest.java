ebf88ecf2ffa8d9609684231766520920cb3ea34
Natalia Petyaeva <npetyaeva@gmail.com>
2021-12-17 12:56:12 +0300

    Modified data test - returned "some data"

--- src/test/java/ru/netology/postmanecho/MobileBankApiTest.java
+++ src/test/java/ru/netology/postmanecho/MobileBankApiTest.java
@@ -20,7 +20,7 @@ public class MobileBankApiTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
