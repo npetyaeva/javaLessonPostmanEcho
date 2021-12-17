3f0f11dbb76a8d5efb699ef5501c1c2fba4df5c5
Natalia Petyaeva <npetyaeva@gmail.com>
2021-12-17 12:54:12 +0300

    Modified data test - change "some data" to "some"

--- src/test/java/ru/netology/postmanecho/MobileBankApiTest.java
+++ src/test/java/ru/netology/postmanecho/MobileBankApiTest.java
@@ -20,7 +20,7 @@ public class MobileBankApiTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("some"))
         ;
     }
 }
